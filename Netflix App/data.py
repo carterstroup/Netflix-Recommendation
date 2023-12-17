fe27c7df488554830c7b0cb67e26127df9ca4220
Carter Stroup <carterstroup@gmail.com>
2023-12-17 13:12:47 -0600

    cleaned, commented, and adjusted: FULL WORKING SEARCH

--- Netflix App/data.py
+++ Netflix App/data.py
@@ -1,1 +1,58 @@
-#placeholder
+#Imports
+import pandas as pd
+
+#Takes a nested array and turns it into a one-dimensional array
+#Runtime: O(n)
+def flatten(list):
+    result = []
+    for item in list:
+        if hasattr(item, "__iter__") and not isinstance(item, str):
+            result.extend(flatten(item))
+        else:
+            result.append(item)
+    return result
+
+#retrieves a list of actors or show names from the csv file and stores them in a list
+#cleans the data in the process by skipping blanks, changing non-values to 'Not Available' and removes duplicates
+#O(n)
+def get_data(type):
+    #gets certain data based on need to reduce runtime
+    if type == "name":
+        df = pd.read_csv("netflix_titles.csv", usecols=['title'], skip_blank_lines=True)
+    elif type == "actor":
+        df = pd.read_csv("netflix_titles.csv", usecols=['cast'], skip_blank_lines=True)
+    df.fillna('Not Available', inplace=True) #replaces non-values to string
+    df_no_duplicates = df.drop_duplicates() #removes duplicates
+    df_to_list = df_no_duplicates.values.tolist() #converts the elements to a nested list, but the entire cell of actors is one string
+    #splits the list by comma, converts it to one dimension, and removes duplicates and whitespace : giving each actor their own index in the list of strings
+    actor_list_flattened = flatten(df_to_list)
+    starting_list = []
+    for item in actor_list_flattened:
+        split = item.split(",")
+        starting_list.append(split)
+    flat_list = flatten(starting_list)
+    final_list = []
+    #removes whitespace in strings and removes duplicates by converting it to a dict and back
+    for item in flat_list:
+        final_list.append(item.strip())
+    no_duplicates_list = list(dict.fromkeys(final_list))
+    return no_duplicates_list
+
+#retrieves a list of shows by either the show name or actors who are in the show
+#type is name/actor indicating search method, and by_method is the string holding the show or actor name
+#Runtime: O(n)
+def get_show(type, by_method):
+    data = pd.read_csv('netflix_titles.csv', skip_blank_lines=True) #gets data
+    data.fillna('Not Available', inplace=True) #changes non-values to string Not Available
+    data_dict = data.to_dict(orient='records') #converts the data frame to a dict
+    shows_list = {}
+    #iterates through all fo the data to form and return a nested dict with all of the show information
+    if type == "name":
+        for show in data_dict:
+            if by_method in show["title"]:
+                shows_list[show["show_id"]] = show
+    elif type == "actor":
+        for show in data_dict:
+            if by_method in show["cast"]:
+                shows_list[show["show_id"]] = show
+    return shows_list
--- Netflix App/main.py
+++ Netflix App/main.py
@@ -1,11 +1,12 @@
 #Imports
 import time
-from data_import import get_data, get_show
+from data import get_data, get_show
 
-#Initialization
-#Asks the user if they wish to use the search or recommendation function and calls the appropriate function
+#Asks the user if they wish to use the search or recommendation feature and 
+#calls the appropriate function using a helper function (get_start_input)
+#Runtime: O(1)
 def program_start(run_num):
-    if run_num == 0:
+    if run_num == 0: #checks if this is the first time the function has been called.
         print("Welcome to the Netflix Search and Recommendation Platform!")
         time.sleep(1)
         print("Would you like to search or get a recommendation?")
@@ -14,7 +15,9 @@ def program_start(run_num):
         print("Please enter 'recommendation' or 'search'.")
         get_start_input()
         
-#gets the user input to assist the program start function            
+#Gets the user input to help the program_start function.
+#It ensures the input is valid, and calls the appropriate function. 
+#Runtime: O(1)         
 def get_start_input():
     movie_or_show_selection = input().strip().lower()
     if movie_or_show_selection == "lookup" or movie_or_show_selection == "look up" or movie_or_show_selection == "search":
@@ -23,9 +26,11 @@ def get_start_input():
         print("This function is still in development, please use the search function by typing 'search'.")
         get_start_input()
     else:
-        return program_start(1)
+        return program_start(1) #the input was not valid, it will start the process over again
 
-#Lookup Function: Allows the user to search Netflix shows based on the name or actors
+#Allows the user to choose between searching by actors or by the name of the show.
+#Uses a helper function (lookup_input_helper) to get user input and validate it.
+#Runtime: O(1)
 def lookup_init(run_num):
     if run_num == 0:
         print("Would you like to search by actors or the name of a show?")
@@ -34,7 +39,8 @@ def lookup_init(run_num):
         print("Please enter 'name' or 'actor'.")
         lookup_input_helper()
 
-#assists with managing the user input for the lookup_init
+#Helps the lookup_init function by getting and validating a user input.
+#Runtime: O(1)
 def lookup_input_helper():
     actors_or_show_name = input().strip().lower()
     if actors_or_show_name == "actor" or actors_or_show_name == "actors" or actors_or_show_name == "search by actors" or actors_or_show_name == "search for actors":
@@ -42,17 +48,25 @@ def lookup_input_helper():
     elif actors_or_show_name == "name" or actors_or_show_name == "show name" or actors_or_show_name == "show":
         lookup("name")
     else:
-        return lookup_init(1)
+        return lookup_init(1) #the input was not valid
 
+#Searches the data sourced in data.py by actor or name (indicated by 'type' argument) depending on user choice earlier in the program.
+#It then passes on the narrowed data to get_selection.
+#Runtime: O(n)
 def lookup(type):
-    working_list = None
-    final_list = []
+    working_list = None #will be assigned a list of actors or show titles based on user search method
+    final_list = [] #the list that will be returned
+    #modifies working_list based on user search option, indicated in the functions arguments
     if type == "name":
         working_list = get_data("name")
         print("Please enter part or all of a show's name.")
     elif type == "actor":
         working_list = get_data("actor")
         print("Please enter part or all of an actor's name.")
+    #runs through all of the data in working_list comparing it to the user input (search).
+    #it will then append the matching data to a list, if the list is greater than 9, the program will ask for a more specific search.
+    #If the list is empty, it will ask for a different search.
+    #If the list is between 1-9, it will pass the data on to get_selection
     while True:
         user_input = input().strip()
         list_of_matches = []
@@ -70,6 +84,12 @@ def lookup(type):
         else:
             final_list += list_of_matches
             break
+    get_selection(type, final_list)
+             
+#takes the list of 1-9 names or show titles from lookup, presents the data for the user to choose, then passes the selection
+#on to list_shows_and_info for the final result to be displayed.
+#Runtime: O(n)
+def get_selection(type, output_list):
     if type == "name":
         print("We have found the following shows from our records.")
         time.sleep(1.2)
@@ -78,17 +98,20 @@ def lookup(type):
         print("We have found the following actors from our records.")
         time.sleep(1.2)
         print("Please enter the number that correlates to the name of your intended actor.")
-    working_dict = {} 
-    idx_track = 0
-    for actor in final_list:
-        working_dict[str(idx_track)] = actor
+    working_dict = {} #will eventually house a number that corresponds to an option passed into the function
+    idx_track = 0 #tracks the index of the list to assign that same number to the key for working_dict
+    #iterates through the list passed in and assigns each to the working_dict
+    for item in output_list:
+        working_dict[str(idx_track)] = item
         idx_track += 1
-    #printing the dictionary in a more readable fashion
+    #Prints the dictionary in user-friendly fashion, rather than in dict form
     for key, value in working_dict.items():
         print(key + ": " + value)
+    #uses the input_helper helper function to validate the user choice as an actual option based on the dict
     final_selection = input_helper(working_dict)
     print("You have selected " + working_dict[final_selection])
     time.sleep(1.2)
+    #finishes the function by calling list_shows_and_info on the final user selection
     if type == "name":
         print("Here is some more information about this show:")
         time.sleep(1.2)
@@ -96,41 +119,45 @@ def lookup(type):
     elif type == "actor":
         print("Here is a list of shows this actor has appeared in.")
         time.sleep(1.2)
-        list_shows_and_info(get_show("actor", working_dict[final_selection]))
+        list_shows_and_info(get_show("actor", working_dict[final_selection]))        
 
-    
+#helps get_selection by validating the input is in the dict and returns the valid choice as a key to the dict
+#Runtime: O(n)
 def input_helper(dict):
     a_input = input().strip()
-    num_options = []
+    num_options = [] #houses keys from passed in dict, which are input options
+    #adds the keys to num_options
     for num in dict:
         num_options.append(num)
+    #checks if the user input matches an option in num_options
     for option in num_options:
         if a_input == option:
             return option
     print("Please enter a valid option.")
     return input_helper(dict)
 
-def list_shows_and_info(show_list):
-    key_list = []
-    for key in show_list:
+#takes an input of a nested dict full of shows with all the show info, and unpacks it one by one for the user to see
+#Runtime: O(n)
+def list_shows_and_info(show_dict):
+    key_list = [] #holds a list of dict keys
+    #appends key_list with the keys of each nested dict
+    for key in show_dict:
         key_list.append(key)
+    #prints out the info in each nested dict using the nested keys in key_list
     for key in key_list:
-        show = show_list
         print("")
         print("---------------------------------------")
-        if show[key]["type"] == "Movie":
-            print("Movie Name: " + show[key]["title"])
+        if show_dict[key]["type"] == "Movie":
+            print("Movie Name: " + show_dict[key]["title"])
         else:
-            print("TV Show Name: " + show[key]["title"])
-        print("Release Year: " + str(show[key]["release_year"]))
-        print("Rating: " + show[key]["rating"])
-        print("Duration: " + show[key]["duration"])
-        print("Genre: " + show[key]["listed_in"])
-        print("Description: " + show[key]["description"])
+            print("TV Show Name: " + show_dict[key]["title"])
+        print("Release Year: " + str(show_dict[key]["release_year"]))
+        print("Rating: " + show_dict[key]["rating"])
+        print("Duration: " + show_dict[key]["duration"])
+        print("Genre: " + show_dict[key]["listed_in"])
+        print("Description: " + show_dict[key]["description"])
         print("---------------------------------------")
         print("")
 
-#Recommendation Function: Suggests shows/movies to the user based on questions asked about ratings, country, genre, etc.
-
-#Function Calls / Application Management
+#Start the program
 program_start(0)
